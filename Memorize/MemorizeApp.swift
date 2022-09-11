8e0c06434d26b303f7e8e7f51e7a6f47f9319c69
irenfeliz <irenfeliz@gmail.com>
2022-09-11 11:48:31 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Irina Zorina on 11.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
